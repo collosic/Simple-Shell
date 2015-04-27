bcd59326e028da89770753e10413d70ef76454b5
collosic <christian.collosi@gmail.com>
2015-04-27 09:59:03 -0700

    added Anna's SIG handling additions

--- shell.c
+++ shell.c
@@ -5,34 +5,40 @@
  *
  * Student Names:   Scott Shin
  *                  Christin Collosi
- *                  Anna Alexander
+ *                  Anna Alexander Lambrix
  *                  Cory Bryce Sauer
  
  */
 
 // Include header files here
 #include "shell.h"
-#define MAXARGS   128
 
-/* function prototypes */
-void eval(char *cmdline);
-int parseline(char *buf, char **argv);
-int builtin_command(char **argv);
 
 int main()
 {
     char cmdline[MAXLINE]; /* Command line */
+    create_proc(all_proc);
+    
+    //Signal handlers with wrapper Signal  installed
+    Signal(SIGINT, int_handler); // for kbd interrupt (for foreground?)
+    Signal(SIGTSTP, tstp_handler); // for terminal z (for foreground?)
+    Signal(SIGCHLD, child_handler); // when child is terminated 
+    
     
     while (1) {
         /* Read */
-        printf("> ");
+        printf("shell53> ");
         Fgets(cmdline, MAXLINE, stdin);
-        if (feof(stdin))
+        if (feof(stdin)) {
             exit(0);
+        }
         
         /* Evaluate */
         eval(cmdline);
+        fflush(stdout); // A: I added that crap bc the shell was acting weird after FG commands... 
+        fflush(stdout);
     }
+    exit(0);
 }
 /* $end shellmain */
 
@@ -45,12 +51,19 @@ void eval(char *cmdline)
     int bg;              /* Should the job run in bg or fg? */
     pid_t pid;           /* Process id */
     
+    //A: this is from textbok for parent-child synchronization - signal set
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    
     strcpy(buf, cmdline);
     bg = parseline(buf, argv);
     if (argv[0] == NULL)
         return;   /* Ignore empty lines */
     
     if (!builtin_command(argv)) {
+        //A:
+        sigprocmask(SIG_BLOCK, &mask, 0);
         if ((pid = Fork()) == 0) {   /* Child runs user job */
             if (execve(argv[0], argv, environ) < 0) {
                 printf("%s: Command not found.\n", argv[0]);
@@ -58,14 +71,23 @@ void eval(char *cmdline)
             }
         }
         
+        if(pid < 0) {
+            printf("something wrong with fork\n");
+            return;
+        }
+        
         /* Parent waits for foreground job to terminate */
         if (!bg) {
+            addprocess(all_proc, pid, 'F'); // A: this is a foreground process
             int status;
             if (waitpid(pid, &status, 0) < 0)
                 unix_error("waitfg: waitpid error");
         }
-        else
-            printf("%d %s", pid, cmdline);
+        else {
+            addprocess(all_proc, pid, 'B'); // A: this is a background process
+            printf("Process # %d %s", pid, cmdline);
+        }
+        sigprocmask(SIG_UNBLOCK, &mask, 0); // unblock sigchld signals
     }
     return;
 }
@@ -73,8 +95,28 @@ void eval(char *cmdline)
 /* If first arg is a builtin command, run it and return true */
 int builtin_command(char **argv)
 {
-    if (!strcmp(argv[0], "quit")) /* quit command */
+    if (!strcmp(argv[0], "quit")) { /* quit command */
+        //A: killing bg processes before quit
+        int i;
+        for(i =0; i<MAXPROCESS; i++){
+            if(all_proc[i].cond == 'B'){
+            printf("before quit killing bg process # %d \n", all_proc[i].pid);
+                pid_t pid = all_proc[i].pid;
+                kill(pid, SIGKILL);
+                all_proc[i].cond = 'N';
+                all_proc[i].pid = 0;
+            
+            }
+        }
         exit(0);
+        
+    }
+    //A: just a bookkeeping built in to see what the heck we have in our struct array...
+    if (!strcmp(argv[0], "all")) {
+        show_all(all_proc);
+        return 1;       
+    }   
+        
     if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
         return 1;
     return 0;                     /* Not a builtin command */
@@ -118,59 +160,149 @@ int parseline(char *buf, char **argv)
 /* $begin forkwrapper */
 pid_t Fork(void) 
 {
-        pid_t pid;
-
-            if ((pid = fork()) < 0)
-                    unix_error("Fork error");
-                return pid;
+    pid_t pid;
+    if ((pid = fork()) < 0)
+        unix_error("Fork error");
+    return pid;
 }
 /* $end forkwrapper */
 
 
 char *Fgets(char *ptr, int n, FILE *stream) 
 {
-        char *rptr;
-
-            if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
-                    app_error("Fgets error");
-
-                return rptr;
+    char *rptr;
+    if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
+    app_error("Fgets error");
+    return rptr;
 }
 /* $begin wait */
 
 
 pid_t Wait(int *status) 
 {
-        pid_t pid;
-
-            if ((pid  = wait(status)) < 0)
-                    unix_error("Wait error");
-                return pid;
+    pid_t pid;
+    if ((pid  = wait(status)) < 0)
+    unix_error("Wait error");
+    return pid;
 }
 /* $end wait */
 
 pid_t Waitpid(pid_t pid, int *iptr, int options) 
 {
-        pid_t retpid;
-
-            if ((retpid  = waitpid(pid, iptr, options)) < 0) 
-                    unix_error("Waitpid error");
-                return(retpid);
+    pid_t retpid;
+    if ((retpid  = waitpid(pid, iptr, options)) < 0) 
+    unix_error("Waitpid error");
+    return(retpid);
 }
 
 /* $begin unixerror */
 void unix_error(char *msg) /* Unix-style error */
 {
-        fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-            exit(0);
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
 }
 /* $end unixerror */
 
 
 void app_error(char *msg) /* Application error */
 {
-        fprintf(stderr, "%s\n", msg);
-            exit(0);
+    fprintf(stderr, "%s\n", msg);
+    exit(0);
 }
 /* $end errorfuns */
 
+/* start signal handler fx's*/
+// this is SIGINT
+void int_handler(int sig){
+    pid_t pid;
+    //printf("we got int\n");
+    int i;
+    for(i = 0; i<MAXPROCESS; i++) {
+        if(all_proc[i].cond== 'F'){
+            pid = all_proc[i].pid;
+            if(pid != 0) {
+                all_proc[i].cond = 'N';
+                all_proc[i].pid = 0;
+            }
+        }
+    }
+    
+    if(pid != 0)
+        kill(pid, SIGINT);
+    return;
+}
+
+//SIGTSTP
+void tstp_handler(int sig){
+    printf("we got tstsp do we need it?\n");
+}
+
+void child_handler(int sig){
+    pid_t pid;
+    int status;
+    while((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+        printf("Process # %d is reaped\n", (int)pid);
+        //A: this is status exit check from textbook... section 8.4
+        if(WIFEXITED(status) | WIFSIGNALED(status)) deleteprocess(all_proc, pid);
+        if(WIFSTOPPED(status)) printf("we gotta do smth with stopped process?\n");
+    }
+    if(errno != ECHILD)
+    unix_error("waitpid err");
+    //sleep(2);
+    return;
+}
+
+handler_t *Signal(int signum, handler_t *handler){
+    struct sigaction action, old_action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = SA_RESTART;
+
+    if(sigaction(signum, &action, &old_action) < 0)
+        unix_error("Signal Error");
+    return(old_action.sa_handler);
+}
+    
+int addprocess(struct process *all_proc, pid_t pid, char cond){
+    if(pid < 1) return 0; // not adding nothing
+    int i;
+    for(i = 0; i<MAXPROCESS; i++){ 
+        if(all_proc[i].pid == 0) { // adding something
+            all_proc[i].pid = pid;
+            all_proc[i].cond = cond;
+            return 1;
+        }
+    }
+    return 0; // not adding, some prob
+}
+
+int deleteprocess(struct process *all_proc, pid_t pid) {
+    if(pid < 1) return 0;
+    int i;
+    for(i = 0; i<MAXPROCESS; i++) {
+        if(all_proc[i].pid == pid) {
+            all_proc[i].pid = 0;
+            all_proc[i].cond = 'N';
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+//creating empty array of structs with 'N' aka None condition
+void create_proc(struct process *all_proc) {
+    int i;
+    for(i = 0; i<MAXPROCESS; i++) {
+        all_proc[i].pid = 0;
+        all_proc[i].cond = 'N';
+    }
+}
+
+//just a bookkeeping fx to see all we have 
+void show_all(struct process *all_proc){
+    for(int i =0; i < MAXPROCESS; i++){
+        if(all_proc[i].pid != 0)
+            printf("we got process  %d  with state of %c\n", all_proc[i].pid, all_proc[i].cond);
+    }
+}
--- shell.h
+++ shell.h
@@ -24,16 +24,40 @@
 #include <arpa/inet.h>
 
 /* Misc constants */
-#define MAXLINE  8192  /* Max text line length */
+#define MAXLINE     8192
+#define MAXARGS     128
+#define MAXLINES    25 // screen display with fixed # of lines (25)
+#define MAXPROCESS  10
+
+/* Type Definitions */
+typedef void handler_t(int);
+
+struct process {
+	pid_t pid;
+	char cond;
+};
 
 /* External variables */
 extern int h_errno;    /* Defined by BIND for DNS errors */ 
 extern char **environ; /* Defined by libc */
 
+/* Global Variables */
+struct process all_proc[MAXPROCESS];
+
 /* function prototypes */
 void eval(char *cmdline);
 int parseline(char *buf, char **argv);
 int builtin_command(char **argv); 
+void create_proc(struct process *all_proc);
+int addprocess(struct process *all_proc, pid_t pid, char cond);
+int deleteprocess(struct process *all_proc, pid_t pid);
+void show_all(struct process *all_proc);
+
+/* SIG Handlers */
+void int_handler(int sig);
+void tstp_handler(int sig);
+void child_handler(int sig);
+handler_t *Signal(int signum, handler_t *handler);// wrapper for handlers
 
 /* Our own error-handling functions */
 void unix_error(char *msg);
