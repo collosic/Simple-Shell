f8d024d72c167b44865b07401c308cc403013910
NicodySazawa <cbsauer@uci.edu>
2015-05-03 17:04:03 -0700

    we.c
    Still not sure if there's anything else to this, but throwing it up there for now

---
+++ we.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAXLINE 8192
+
+int main {
+    
+    char *str[MAXLINE]
+  
+    scanf("%[^\n]%*c",str);
+    printf("This is from stdin: %s\n",str);
+    
+    return 0;
+
+}    
